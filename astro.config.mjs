bba754d97ae219b26c74b1989ba3acab0dac4e54
costacode <jscartas@gmail.com>
2023-05-21 11:35:19 +0100

    Revert "config astro"
    This reverts commit 8e1be538c5ff88a827a10995d7383f1dcfe51c5c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://costacode.github.io",
-  base: '/ttlanding',
+  site: "https://astro-moon-landing.netlify.app/",
   integrations: [
     tailwind(),
     image({
