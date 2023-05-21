fae5ba7ee84b871007c9601cbfc523673c3b443c
costacode <jscartas@gmail.com>
2023-05-21 12:06:07 +0100

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://tt-landing.onrender.com/",
+  // base: '/ttlanding',
 
   integrations: [
     tailwind(),
